ff7ca176e2d3e2f2143fef764f89fa74bfb84bf3
snissim123 <sandra.nissim@gmail.com>
2019-12-08 22:27:44 -0600

    ui display

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {FormControl, CardHeader, CardContent, CardMedia, Container} from '@material-ui/core';
+import {FormControl, CardHeader, CardContent, CardMedia, Container, Typography} from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
@@ -10,7 +10,7 @@ const cardStyles = makeStyles(theme => ({
     marginTop: 100,   
     paddingLeft: 60,
     paddingRight: 60,
-    marginLeft: 245,
+    marginLeft: 20,
   },
   card:{
     display:"flex",
@@ -27,6 +27,13 @@ const cardStyles = makeStyles(theme => ({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
+  },
+  button:{
+    backgroundColor: "black",
+    color: 'white',
+    width: 260,
+    height: 50,
+    marginTop: 30,
   }
 
 }));
@@ -45,28 +52,22 @@ const App = () => {
   }, []);
 
   return (
-    // <ul>
-    //   {products.map(product => 
-    //     <Card textAlign="centered">
-    //       <img src={"data/products/"+product.sku+"_1.jpg"} height="80" width="55"></img>
-    //       <li key={product.sku}>{product.title}</li>
-    //     </Card>        
-    //     )}
-    // </ul>
-
-
-  <Grid container spacing={3} className={classes.grid}>       
-        {products.map(product => 
-          (<Grid item xs={4}>
-            <Card key={product.sku} className={classes.card}>
-              <CardMedia><img src={"data/products/"+product.sku+"_1.jpg"} height="200" width="137.5"></img></CardMedia>
-              <CardContent className={classes.content}>
-                {product.title}
-                <Button variant="contained" color="primary" size="large">View Doctor Bio</Button>
-              </CardContent>
-            </Card>
-        </Grid>))}
-       </Grid>
+  <div>
+    <Typography className={classes.grid}>{products.length} product(s) found.</Typography>
+    <Grid container spacing={3} className={classes.grid}>       
+          {products.map(product => 
+            (<Grid item xs={4}>
+              <Card key={product.sku} className={classes.card}>
+                <CardMedia><img src={"data/products/"+product.sku+"_1.jpg"} height="200" width="137.5"></img></CardMedia>
+                <CardContent className={classes.content}>
+                  {product.title}
+                  <Typography >${product.price}</Typography>
+                  <Button variant="contained" className={classes.button} size="medium">Add to cart</Button>
+                </CardContent>
+              </Card>
+          </Grid>))}
+        </Grid>
+      </div>
   );
 };
 
